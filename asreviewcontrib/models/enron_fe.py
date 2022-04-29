f29d08ed43a238a62743265da81921bc14f6cdd4
michagast <45239639+michagast@users.noreply.github.com>
2022-04-29 10:50:35 +0200

    test

--- asreviewcontrib/models/enron_fe.py
+++ asreviewcontrib/models/enron_fe.py
@@ -1,5 +1,9 @@
 
 from asreview.models.feature_extraction.base import BaseFeatureExtraction
+import pandas as pd                 #For data science purposes
+import re                           #For performing regex
+import torch                        #For running models with cude
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 class Enron(BaseFeatureExtraction):
     """Naive Bayes classifier
@@ -13,3 +17,9 @@ class Enron(BaseFeatureExtraction):
     def __init__(self):
 
         super(Enron, self).__init__()
+        self._model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english" )
+        self._model.eval()
+        self._tokenizernlp = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+    def transform(self, texts):
+        X = self._model.transform(texts).tocsr()
+        return X

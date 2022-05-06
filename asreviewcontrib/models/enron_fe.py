91390200818c8bd47b6db4b44fe6bcbb68049150
michagast <45239639+michagast@users.noreply.github.com>
2022-05-06 12:28:50 +0200

    update

--- asreviewcontrib/models/enron_fe.py
+++ asreviewcontrib/models/enron_fe.py
@@ -108,13 +108,13 @@ class Enron(BaseFeatureExtraction):
         '''
         tokens = [x for x in self.split_into_sentences(text) if not any(y in x for y in ['/','+'])]  # split text into sentences and remove any sentence that contains / or + as a character
         tokens = self.remove_short_tokens(tokens)
+        print(tokens)
         if tokens:
             inputs = self._tokenizerner.batch_encode_plus(tokens, return_tensors="pt", padding=True, max_length=512,truncation=True)  # tokenize sentences, max_length is 512 for if cuda is enabled to speed the model up
             with torch.no_grad():
                 results = self._modelner(**inputs)
-                if results:
-                    for i, input in enumerate(inputs['input_ids']):
-                        namedentities = [self._modelner.config.id2label[item.item()] for item in results.logits[i].argmax(axis = 1)]  # for every probability for a named entity for a word, turn the probabilities into their associated labels
+                for i, input in enumerate(inputs['input_ids']):
+                namedentities = [self._modelner.config.id2label[item.item()] for item in results.logits[i].argmax(axis = 1)]  # for every probability for a named entity for a word, turn the probabilities into their associated labels
             entitynumberlist = self.generate_entity_list(namedentities)  # Based on the array of entity names that is generated, count each entity and make a dict of this
         else:
             entitynumberlist = [0,0,0,0,0,0,0]

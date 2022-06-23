f750a86c4df434bd32ba80a8684be02cf38c3172
michagast <45239639+michagast@users.noreply.github.com>
2022-06-23 15:48:20 +0200

    changed requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     keywords='systematic review',
     packages=find_namespace_packages(include=['asreviewcontrib.*']),
-    python_requires='~=3.6',
+    python_requires='~=3.10',
     install_requires=[
         'sklearn',
         'asreview>=0.13',

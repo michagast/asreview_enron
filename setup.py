a228d784b5be0167a9985279a970c41f00be3310
michagast <45239639+michagast@users.noreply.github.com>
2022-06-27 12:30:28 +0200

    small change

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'pyenchant',
         'nltk',
         'passivepy',
-        'https://raw.githubusercontent.com/mitramir55/PassivePy/main/PassivePyCode/PassivePySrc/requirements_lg.txt',
+        'git+https://raw.githubusercontent.com/mitramir55/PassivePy/main/PassivePyCode/PassivePySrc/requirements_lg.txt',
 
     ],
     entry_points={

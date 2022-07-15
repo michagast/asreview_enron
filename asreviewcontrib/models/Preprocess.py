69ecacb25cb75dc6dabbc4af7138df5dd935f4ff
michagast <45239639+michagast@users.noreply.github.com>
2022-07-15 12:49:02 +0200

    changes

--- asreviewcontrib/models/Preprocess.py
+++ asreviewcontrib/models/Preprocess.py
@@ -3,6 +3,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from num2words import num2words
+import nltk
 
 
 nltk.download('punkt')

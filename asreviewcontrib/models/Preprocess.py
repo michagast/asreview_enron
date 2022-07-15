95cc2ede6d0583b9554733c4e85fe7a7f3819c15
michagast <45239639+michagast@users.noreply.github.com>
2022-07-15 12:54:26 +0200

    temporarily moved preprocessing

--- asreviewcontrib/models/Preprocess.py
+++ asreviewcontrib/models/Preprocess.py
@@ -9,63 +9,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-def convert_lower_case(text):
-    return np.char.lower(text)
 
-def remove_stop_words(text):
-    stop_words = stopwords.words('english')
-    words = word_tokenize(str(text))
-    new_text = ""
-    for w in words:
-        if w not in stop_words and len(w) > 1:
-             new_text = new_text + " " + w
-    return new_text
-
-def remove_punctuation(text):
-    symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
-    for i in range(len(symbols)):
-        text = np.char.replace(text, symbols[i], ' ')
-        text = np.char.replace(text, "  ", " ")
-    text = np.char.replace(text, ',', '')
-    return text
-
-def remove_apostrophe(text):
-    return np.char.replace(text, "'", "")
-
-def stemming(text):
-    stemmer = PorterStemmer()
-
-    tokens = word_tokenize(str(text))
-    new_text = ""
-    for w in tokens:
-        new_text = new_text + " " + stemmer.stem(w)
-    return new_text
-
-def convert_numbers(text):
-    tokens = word_tokenize(str(text))
-    new_text = ""
-    for w in tokens:
-        try:
-            w = num2words(int(w))
-        except:
-            a = 0
-        new_text = new_text + " " + w
-    new_text = np.char.replace(new_text, "-", " ")
-    return new_text
-
-def preprocess(text):
-    text = convert_lower_case(text)
-    text = remove_punctuation(text)  # remove comma seperately
-    text = remove_apostrophe(text)
-    text = remove_stop_words(text)
-    text = convert_numbers(text)
-    text = stemming(text)
-    text = remove_punctuation(text)
-    text = convert_numbers(text)
-    text = stemming(text)  # needed again as we need to stem the words
-    text = remove_punctuation(text)  # needed again as num2word is giving few hypens and commas fourty-one
-    text = remove_stop_words(text)  # needed again as num2word is giving stop words 101 - one hundred and one
-    return text
 
 
 
--- asreviewcontrib/models/enron_fe.py
+++ asreviewcontrib/models/enron_fe.py
@@ -10,8 +10,12 @@ from nltk.tokenize import word_tokenize
 from collections import Counter
 #from nltk.tag import pos_tag        #For finding proper nouns in text
 from PassivePySrc import PassivePy  #For detecting passive voice in sentences
-from asreviewcontrib.models.Preprocess import Preprocess
 from tensorflow.keras.preprocessing.text import hashing_trick
+rom nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from num2words import num2words
+
 
 
 
@@ -51,6 +55,7 @@ class Enron(BaseFeatureExtraction):
 
 
         nltk.download('punkt')
+        nltk.download('stopwords')
         #nltk.download('averaged_perceptron_tagger')
 
 
@@ -291,8 +296,8 @@ class Enron(BaseFeatureExtraction):
         processed_text_tf_idf = []
         processed_text_bow = []
         for text in texts:
-            processed_text_tf_idf.append(word_tokenize(str(Preprocess.preprocess(self, text))))
-            processed_text_bow.append(str(Preprocess.preprocess(self, text)))
+            processed_text_tf_idf.append(word_tokenize(str(self.preprocess(self, text))))
+            processed_text_bow.append(str(self.preprocess(self, text)))
 
         tf_idf_values = self.tf_idf(processed_text_tf_idf)
 
@@ -375,3 +380,61 @@ class Enron(BaseFeatureExtraction):
         ''' Function that caclulates what percentage of sentences in a text are written in active voice. This is done using the passivePy package.
         '''
         return (1 - self.percentage_passive_voice(text))
+
+    def convert_lower_case(text):
+        return np.char.lower(text)
+
+    def remove_stop_words(self,text):
+        stop_words = stopwords.words('english')
+        words = word_tokenize(str(text))
+        new_text = ""
+        for w in words:
+            if w not in stop_words and len(w) > 1:
+                new_text = new_text + " " + w
+        return new_text
+
+    def remove_punctuation(self,text):
+        symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+        for i in range(len(symbols)):
+            text = np.char.replace(text, symbols[i], ' ')
+            text = np.char.replace(text, "  ", " ")
+        text = np.char.replace(text, ',', '')
+        return text
+
+    def remove_apostrophe(sefl,text):
+        return np.char.replace(text, "'", "")
+
+    def stemming(sefl,text):
+        stemmer = PorterStemmer()
+
+        tokens = word_tokenize(str(text))
+        new_text = ""
+        for w in tokens:
+            new_text = new_text + " " + stemmer.stem(w)
+        return new_text
+
+    def convert_numbers(self,text):
+        tokens = word_tokenize(str(text))
+        new_text = ""
+        for w in tokens:
+            try:
+                w = num2words(int(w))
+            except:
+                a = 0
+            new_text = new_text + " " + w
+        new_text = np.char.replace(new_text, "-", " ")
+        return new_text
+
+    def preprocess(self,text):
+        text = self.convert_lower_case(text)
+        text = self.remove_punctuation(text)  # remove comma seperately
+        text = self.remove_apostrophe(text)
+        text = self.remove_stop_words(text)
+        text = self.convert_numbers(text)
+        text = self.stemming(text)
+        text = self.remove_punctuation(text)
+        text = self.convert_numbers(text)
+        text = self.stemming(text)  # needed again as we need to stem the words
+        text = self.remove_punctuation(text)  # needed again as num2word is giving few hypens and commas fourty-one
+        text = self.remove_stop_words(text)  # needed again as num2word is giving stop words 101 - one hundred and one
+        return text
